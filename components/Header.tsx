e6d594dc17a30a64a766971698c005d163b17f51
ameeenamzz <shoukethameen@gmail.com>
2025-11-08 18:46:59 +0530

    added shadcn/ui

---
+++ components/Header.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Button } from "./ui/button";
+
+const Header = () => {
+  return (
+    <div>
+      <h1>This is Header component</h1>
+      <Button variant="outline">Submit</Button>
+    </div>
+  );
+};
+
+export default Header;
